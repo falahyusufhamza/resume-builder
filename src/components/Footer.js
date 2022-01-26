dfc9ccbb15634a45f3c4f8dbf403064f052ce888
falah.yusuf <falah.yusuf@lenoksolutions.com>
2022-01-26 11:54:33 +0400

    theme added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,9 @@ import {Navbar} from 'react-bootstrap'
 
 function Footer() {
   return <div style={{bottom : 0, left : 0,position : 'fixed',width :'100%'}}>
-  <Navbar bg="light" expand="lg">
-<Navbar.Brand href="#home">Powered By Mohammed Falah Solutions 2022</Navbar.Brand>
-<Navbar.Toggle aria-controls="basic-navbar-nav" />
-<Navbar.Collapse id="basic-navbar-nav">
-</Navbar.Collapse>
+  <Navbar bg="light">
+<Navbar.Brand style={{fontSize : 15}}>
+Powered By Mohammed Falah Solutions 2022</Navbar.Brand>
 </Navbar>
 </div>;
 }
---
+++ src/theme/theme.js
@@ -0,0 +1,26 @@
+const Theme = {
+    default: {
+        primary: '#16A8A1',
+        secondary: '#596359',
+        primaryDark: '#007873',
+        secondaryDark: '#303930',
+        primaryLight: '#5edad2',
+        secondaryLight: '#869086',
+        darkColor: '#000000',
+        lightColor: '#ffffff',
+        bgColor : '#ffffff'
+      },
+      dark: {
+        primary: '#141a32',
+        secondary: '#3e65fb',
+        primaryDark: '#007873',
+        secondaryDark: '#303930',
+        primaryLight: '#5a6583',
+        secondaryLight: '#809cf5',
+        darkColor: '#000000',
+        lightColor: '#d2d2d7',
+        bgColor : '#141a32'
+      },
+}
+
+export default Theme;
