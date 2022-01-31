fb456d6bf9cf9fea6eb8fc279c908d66dc47d3e7
falah.yusuf <falah.yusuf@lenoksolutions.com>
2022-01-31 21:09:28 +0400

    theme change

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,11 +1,11 @@
 const Theme = {
     default: {
-        primary: '#16A8A1',
-        secondary: '#596359',
-        primaryDark: '#007873',
-        secondaryDark: '#303930',
-        primaryLight: 'rgb(30 181 171)',
-        secondaryLight: '#869086',
+        primary: '#182039',
+        secondary: '#rgb(62, 101, 251)',
+        primaryDark: '#4a4d50',
+        secondaryDark: '#rgb(19 34 94)',
+        primaryLight: '#182039',
+        secondaryLight: '#rgb(195 205 246)',
         darkColor: '#000000',
         lightColor: '#ffffff',
         bgColor : '#ffffff'
