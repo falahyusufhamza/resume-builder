30b2c6aabbcefaa2cf43514d368772cb5094de51
falah.yusuf <falah.yusuf@lenoksolutions.com>
2022-01-31 16:52:01 +0400

    minor changes

--- src/pages/Templates.js
+++ src/pages/Templates.js
@@ -3,12 +3,28 @@ import { Button, Carousel } from 'react-bootstrap';
 import Theme from '../theme/theme';
 import ThemeContext from '../theme/ThemeContext';
 import defaultTemplate from '../assets/Default-template.png'
-import { useDispatch } from 'react-redux';
-import {templateActions} from '../store/store'
+import { useDispatch, useSelector } from 'react-redux';
+import {templateActions,userActions} from '../store/store'
+import { useHistory } from 'react-router-dom';
 
 function Templates() {
+    const userState = useSelector(state => state.userReducer);
+    const history = useHistory();
     const dispatch = useDispatch();
     const themeContext = useContext(ThemeContext);
+
+
+    const onSelectTemplate = (template) => {
+        dispatch(templateActions.switchTemplate({template : 'default'}))
+        if(userState.summary !== ''){
+            history.replace('/view-resume')
+        }
+        else{
+            history.replace('/build-resume');
+        }
+    }
+
+
   return <div style={{height : '100%',width : '100%',margin : 0,padding : 5,backgroundColor : Theme[themeContext.theme].bgColor,display : 'flex', justifyContent : 'center'}}>
 <Carousel style={{height : 500, width : 300}}>
   <Carousel.Item>
@@ -19,7 +35,7 @@ function Templates() {
     />
     <Carousel.Caption>
       <h5 style={{color : 'black'}}>Default Template</h5>
-      <Button onClick={() => dispatch(templateActions.switchTemplate({template : 'default'}))}>Select</Button>
+      <Button onClick={() => onSelectTemplate('default')}>Select</Button>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
